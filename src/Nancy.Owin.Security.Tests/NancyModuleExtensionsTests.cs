acb61d184d22e185954d6284c3b3b39a80564ae0
Damian Hickey <dhickey@gmail.com>
2013-08-17 22:52:00 +0100

    Login test

--- src/Nancy.Owin.Security.Tests/NancyModuleExtensionsTests.cs
+++ src/Nancy.Owin.Security.Tests/NancyModuleExtensionsTests.cs
@@ -1,20 +1,25 @@
 ﻿namespace Nancy.Owin.Security.Tests
 {
-    using System.Net;
+    using System.Collections.Generic;
     using System.Net.Http;
+    using System.Security.Principal;
     using System.Threading.Tasks;
     using FluentAssertions;
     using global::Owin;
     using global::Owin.Testing;
     using Microsoft.Owin;
     using Microsoft.Owin.Infrastructure;
+    using Microsoft.Owin.Security;
     using Microsoft.Owin.Security.Cookies;
+    using Nancy.ModelBinding;
     using Xunit;
+    using HttpStatusCode = Nancy.HttpStatusCode;
 
     public class NancyModuleExtensionsTests
     {
-        [Fact]
-        public async Task When_attempt_to_access_secure_area_then_should_redirect_to_login_page()
+        private readonly HttpClient _httpClient;
+
+        public NancyModuleExtensionsTests()
         {
             OwinTestServer testServer = OwinTestServer.Create(builder =>
             {
@@ -22,16 +27,64 @@
                 var cookieAuthenticationOptions = new CookieAuthenticationOptions
                 {
                     LoginPath = new PathString("/login"),
-                    LogoutPath = new PathString("/logout")
+                    LogoutPath = new PathString("/logout"),
                 };
                 builder.UseCookieAuthentication(cookieAuthenticationOptions);
                 builder.UseNancy();
             });
+            _httpClient = testServer.CreateHttpClient();
+        }
 
-            HttpClient httpClient = testServer.CreateHttpClient();
-
-            HttpResponseMessage response = await httpClient.GetAsync("http://example.com/secured");
+        [Fact]
+        public async Task When_attempt_to_access_secure_area_then_should_redirect_to_login_page()
+        {
+            HttpResponseMessage response = await _httpClient.GetAsync("http://example.com/secured");
             response.StatusCode.Should().Be(HttpStatusCode.Found);
+        }
+
+        [Fact]
+        public async Task When_login_then_should_get_auth_cookie()
+        {
+            HttpResponseMessage response = await _httpClient.PostAsync("http://example.com/login", new FormUrlEncodedContent(new[]
+            {
+                new KeyValuePair<string, string>("Username", "user"), 
+                new KeyValuePair<string, string>("Password", "pass")
+            }));
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+        }
+
+        public class TestModule : NancyModule
+        {
+            public TestModule()
+            {
+                Get["/"] = _ => "Home page";
+                Get["/secured"] = _ => HttpStatusCode.Unauthorized;
+                Post["/login"] = _ =>
+                {
+                    var login = this.Bind<Login>();
+                    if (login.Username == "user" && login.Password == "pass")
+                    {
+                        var authentication = Context.GetAuthenticationManager();
+                        authentication.SignIn(
+                            new AuthenticationProperties { RedirectUri = "/secured" },
+                            new GenericIdentity("User"));
+                        return null; // umm ...
+                    }
+                    return HttpStatusCode.Unauthorized;
+                };
+                Get["/logout"] = _ =>
+                {
+                    Context.GetAuthenticationManager().SignOut();
+                    return Response.AsRedirect("/");
+                };
+            }
+
+            private class Login
+            {
+                public string Username { get; set; }
+
+                public string Password { get; set; }
+            }
         }
     }
 }
