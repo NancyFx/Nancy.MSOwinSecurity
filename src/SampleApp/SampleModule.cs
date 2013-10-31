d76faa7a966096ad57a864545cab9de2023028ab
Damian Hickey <dhickey@gmail.com>
2013-10-31 14:30:55 +0000

    Some work on sample login app

--- src/SampleApp/SampleModule.cs
+++ src/SampleApp/SampleModule.cs
@@ -1,12 +1,7 @@
 ﻿namespace SampleApp
 {
-    using System.Security.Principal;
-    using Microsoft.Owin.Security;
-    using Microsoft.Owin.Security.Cookies;
     using Nancy;
-    using Nancy.ModelBinding;
     using Nancy.Owin.Security;
-    using Nancy.Responses;
     using SampleApp.Models;
 
     public class SampleModule : NancyModule
@@ -15,51 +10,30 @@
         {
             Get["/"] = _ =>
             {
-                var owinAuth = Context.GetAuthenticationManager();
+                var authManager = Context.GetAuthenticationManager();
                 var pageBase = new PageBase();
-                if (owinAuth.User != null && owinAuth.User.Identity.IsAuthenticated)
+                if (authManager.User != null && authManager.User.Identity.IsAuthenticated)
                 {
-                    pageBase.Username =  owinAuth.User.Identity.Name;
+                    pageBase.Username =  authManager.User.Identity.Name;
                 }
                 return View["Index", pageBase];
             };
 
-            Get["/cookies"] = _ => View["Cookies"];
-
-            Post["/cookies"] = _ =>
-            {
-                var login = this.Bind<Login>();
-                if (login.Username == "user" && login.Password == "pass")
-                {
-                    var owinAuth = Context.GetAuthenticationManager();
-                    owinAuth.SignIn(new AuthenticationProperties(),
-                        new GenericIdentity("User", CookieAuthenticationDefaults.AuthenticationType));
-                    return new RedirectResponse("/");
-                }
-                return System.Net.HttpStatusCode.Unauthorized;
-            };
-
             Get["/secured"] = _ =>
             {
-                var owinAuth = Context.GetAuthenticationManager();
-                if (owinAuth.User == null || !owinAuth.User.Identity.IsAuthenticated)
+                var authManager = Context.GetAuthenticationManager();
+                if (authManager.User == null || !authManager.User.Identity.IsAuthenticated)
                 {
                     return HttpStatusCode.Unauthorized;
                 }
                 return HttpStatusCode.OK;
             };
+
             Get["/signout"] = _ =>
             {
                 Context.GetAuthenticationManager().SignOut();
                 return Response.AsRedirect("/");
             };
-        }
-
-        private class Login
-        {
-            public string Username { get; set; }
-
-            public string Password { get; set; }
         }
     }
 }
--- src/SampleApp/Startup.cs
+++ src/SampleApp/Startup.cs
@@ -3,6 +3,8 @@
     using Microsoft.Owin;
     using Microsoft.Owin.Infrastructure;
     using Microsoft.Owin.Security.Cookies;
+    using Microsoft.Owin.Security.Jwt;
+    using Microsoft.Owin.Security.OAuth;
     using Owin;
 
     public class Startup
@@ -10,12 +12,19 @@
         public void Configuration(IAppBuilder builder)
         {
             SignatureConversions.AddConversions(builder);
-            builder.UseCookieAuthentication(new CookieAuthenticationOptions
-            {
-               LoginPath = new PathString("/cookies"),
-               LogoutPath = new PathString("/logout"),
-            });
-            builder.UseNancy(opt => opt.Bootstrapper = new SampleAppBootstrapper());
+            builder
+                .UseCookieAuthentication(new CookieAuthenticationOptions
+                {
+                    LoginPath = new PathString("/cookies"),
+                    LogoutPath = new PathString("/logout"),
+                })
+                .UseMicrosoftAccountAuthentication("000000004810984D", "tmUEupwT4jLWg8XRbPhEbIyi0ZNPMTKk")
+                .UseFacebookAuthentication("appid", "appsecret")
+                .UseGoogleAuthentication()
+                .UseTwitterAuthentication("consumerkey", "consumersecret")
+                .UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions())
+                .UseJwtBearerAuthentication(new JwtBearerAuthenticationOptions())
+                .UseNancy(opt => opt.Bootstrapper = new SampleAppBootstrapper());
         }
     }
 }
