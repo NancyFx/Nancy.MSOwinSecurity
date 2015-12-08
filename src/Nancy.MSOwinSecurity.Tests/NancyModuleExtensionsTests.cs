094bcf10f23f2697e0e7b7b685ac28224b9003ca
Damian Hickey <dhickey@gmail.com>
2015-12-08 08:18:30 +0100

    Unused using directive.

--- src/Nancy.MSOwinSecurity.Tests/NancyModuleExtensionsTests.cs
+++ src/Nancy.MSOwinSecurity.Tests/NancyModuleExtensionsTests.cs
@@ -4,7 +4,6 @@
     using System.Collections.Generic;
     using System.Linq;
     using System.Security.Claims;
-    using System.Security.Principal;
     using System.Threading;
     using FluentAssertions;
     using Nancy.Owin;
